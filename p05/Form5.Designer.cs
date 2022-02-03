bba24411d11778712f54ccab6cd61913fd8804de
Pavlon <pavlonk.04@spst.eu>
2022-02-03 20:58:34 +0100

    pr5

---
+++ p05/Form5.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace p05
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.output = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.execute = new System.Windows.Forms.Button();
+            this.output.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "je",
+            "mu",
+            "hodně",
+            "není",
+            "mu",
+            "nic"});
+            this.listBox1.Location = new System.Drawing.Point(72, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 100);
+            this.listBox1.TabIndex = 0;
+            // 
+            // output
+            // 
+            this.output.Controls.Add(this.label3);
+            this.output.Controls.Add(this.label2);
+            this.output.Controls.Add(this.label1);
+            this.output.Controls.Add(this.textBox3);
+            this.output.Controls.Add(this.textBox2);
+            this.output.Controls.Add(this.textBox1);
+            this.output.Location = new System.Drawing.Point(198, 41);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(214, 115);
+            this.output.TabIndex = 1;
+            this.output.TabStop = false;
+            this.output.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "5";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "10";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "15";
+            // 
+            // execute
+            // 
+            this.execute.Location = new System.Drawing.Point(418, 86);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(75, 23);
+            this.execute.TabIndex = 2;
+            this.execute.Text = "execute";
+            this.execute.UseVisualStyleBackColor = true;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 205);
+            this.Controls.Add(this.execute);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form5";
+            this.Text = "Form1";
+            this.output.ResumeLayout(false);
+            this.output.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox output;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button execute;
+    }
+}
+
---
+++ p05/Form5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p05
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void execute_Click(object sender, EventArgs e)
+        {
+            int soucet = 0;
+            int pocet = 0;
+            double prumer;
+            int value;
+            foreach (Control ctrl in output.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    value = Convert.ToInt32(((TextBox)ctrl).Text);
+                    soucet += value;
+                    pocet++;
+                }
+                if (ctrl is Label)
+                {
+                    string selected = listBox1.GetItemText(listBox1.SelectedItem);
+                    (ctrl as Label).Text = selected;
+                }
+            }
+            if (pocet > 0)
+            {
+                prumer = soucet / (double)pocet;
+                MessageBox.Show("Aritmetický průměr " + prumer);
+            }
+        }
+    }
+}
